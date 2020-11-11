00b3cd79a719810b041d04a267aa1137a2f33cc7
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-11 11:16:37 +0100

    done with push and to see in the page

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ var app = new Vue({
     data: {
         indexList: 0,
         title: 'List-to-do',
-        lists : ['wake-up', 'teorical boolean', 'lunch', 'practical boolean', 'sleep', 'repeat']
+        lists : ['wake-up', 'teorical boolean', 'lunch', 'practical boolean', 'sleep', 'repeat'],
+        inserted: ""
+    },
+    methods : {
+        addToDo() {
+            this.lists.push(this.inserted)
+        }
     }
 });
