64b434829217296ac51da7723ec5042d7ca0fb44
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-11 10:49:49 +0100

    done with list to do

---
+++ main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el : '#root',
+    data: {
+        indexList: 0,
+        title: 'List-to-do',
+        lists : ['wake-up', 'teorical boolean', 'lunch', 'practical boolean', 'sleep', 'repeat']
+    }
+});
